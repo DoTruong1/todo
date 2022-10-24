082853055e2a585901b0942b9d72713a7d7e3e0e
Doooooooooooo λλλλλλ <90887415+DoTruong1@users.noreply.github.com>
2022-10-25 00:33:15 +0700

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,23 @@ import Job from './Jobs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import{BiTrash} from 'react-icons/bi'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const INITIALIZEJOBS = [
-  "Job1",
-  "Job2",
-  "Job3"
+  {
+    id: '1',
+    content: "Job1"
+  },
+  {
+    id: '2',
+    content: "Job2"
+  },
+  {
+    id: '3',
+    content: "Job3"
+  },
 ]
 const App = () => {
   
@@ -32,10 +43,12 @@ const App = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log(job)
     // alert(`The name you entered was: ${job}`)
     var updatedList = [...cardList];
+    var newJob = {id: uuidv4(), content: job}
     if (job) {
-      updatedList.splice(0,0,job)
+      updatedList.splice(0,0,newJob)
       // setCardList((cardList) => [...updatedList, job])
       setCardList(cardList => updatedList)
     }
@@ -50,50 +63,73 @@ const App = () => {
     }
   }
 
-  const listGen = () => {
-      return  <ol className = "list-group list-group-flush "> 
-        {cardList.map((item, index) => 
-          <React.Fragment>
-            <li 
-              key = {index}
-              className="container list-group-item"
-              style={{display: "flex"}}
-            >
-              
-                <Job 
-                  // key={index}
-                  title={item}
-                  index = {index}
-                  handleCheck={handleCheck}
-                  handleDelete = {handleDelete}
-                ></Job>
-            </li>
-            {/* <Button variant="danger primary" size="sm" onClick={() => handleDelete(index)}><BiTrash></BiTrash></Button> */}
-            </React.Fragment>
-          )}
-      </ol>
-    }
-     
+
+  const listGen = cardList.map((item, index) => 
+          <Draggable key = {item.id} draggableId={item.id.toString()} index ={index}>
+                {(provided) => (
+                  <li
+                    className = ""
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}> 
+                   <Job
+                      key={item.id}
+                      item={item}
+                      index = {index}
+                      handleCheck={handleCheck}
+                      handleDelete = {handleDelete}
+                    ></Job>   
+                  </li>
+                 
+                    
+                   
+                )}
+            </Draggable>
+                    
+      )
 
 
+  const handleOnDragEnd = (res) => {
+    const items = [...cardList];
+    const [reorderedItem] = items.splice(res.source.index, 1);
+    items.splice(res.destination.index, 0, reorderedItem);
+
+    setCardList(items);
+  }
   return (
     <div className = "app">
       <form id = "input-job" onSubmit={handleSubmit}>
         <input 
               type="text"
               value = {job}
-              class="form-control" 
+              className = "form-control" 
               onChange = {(event) => setJob(event.target.value)} 
               placeholder="What do you want to do ;)" 
               aria-label="Recipient's username" 
               aria-describedby="button-addon2">
         </input>
-        <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
+        <button className = "btn btn-outline-secondary" type="submit" id="button-addon2">Add</button>
 
       </form>
-      
+      {/* {listGen()} */}
       <div id = "content">
-        {listGen()}
+        <DragDropContext id = "content" onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId = "droppable">
+          {(provided, snapshot) => (
+            
+            <ol
+              className = "list-group list-group-flush"
+
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style = {{width:"100%"}}
+            >
+              {listGen}
+              {provided.placeholder}
+            </ol>
+          )}
+        </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
--- src/Jobs.js
+++ src/Jobs.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import{BiTrash} from 'react-icons/bi'
 import './App.css';
+import { Draggable } from 'react-beautiful-dnd';
 
-const Job = ({title, handleCheck, index, handleDelete}) => {
+
+
+
+
+const Job = ({item, handleCheck, index, handleDelete}) => {
     return (
-        <React.Fragment>
+        // <Draggable key = {item.id} draggableId={item.id.toString()} index ={index}>
+        //         {(provided) => (
+        //         <Container
+        //             ref={provided.innerRef}
+        //             {...provided.draggableProps}
+        //             {...provided.dragHandleProps}
+        //             style = {{display:'flex'}}
+        //         >
+            <React.Fragment>
                     <input 
-                        className = "form-check-input me-1"
-                        value = {title} 
-                        type = "checkbox"
-                        onChange = { () => handleCheck(index)}     
-                    />
-                    <span id ="detail">{title}</span>
-                    <Button id = "trash-icon" variant="danger primary" size="sm" onClick={() => handleDelete(index)}><BiTrash></BiTrash></Button>
+                    className = "form-check-input me-1"
+                    value = {item.content} 
+                    type = "checkbox"
+                    style={{ alignSelf :"center"}}
+                    // {...[provied.draggableProps]}
+                    // {...provied.dragHandleProps}
+                    // innerRef = {provied.innerRef}
+                    onChange = { () => handleCheck(index)}     
+                />
+                <span id ="detail">{item.content}</span>
+                <Button id = "trash-icon" variant="danger primary" size="sm" onClick={() => handleDelete(index)}><BiTrash></BiTrash></Button>
+                </React.Fragment>
+            //     </Container>   
+            //     )}
+            // </Draggable>
 
-        </React.Fragment>
     );
 }
  
